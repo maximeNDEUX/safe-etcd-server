c778bb14f0905bb1a1fcda82ebdcc290f2756bef
Maxime <maxime.mercier@hotmail.com>
2026-02-18 10:52:03 +0100

    edited cypress.config.js module.exports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  allowCypressEnv: false,
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   projectId: "esbonb",
+  video: true, // Active l'enregistrement vidéo
+  videoCompression: 32, // Optionnel : compresse pour réduire la taille (0 à 51)
+  videosFolder: "cypress/videos", // Tu peux même changer le dossier ici
+  screenshotOnRunFailure: true,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
